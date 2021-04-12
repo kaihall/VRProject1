8ff0a5b50aab8d9d12262af905ca020339370fd5
kaihall <49812013+kaihall@users.noreply.github.com>
2021-04-12 00:12:59 -0400

    Initial Commit

---
+++ Assets/Scripts/FlockCountinator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FlockCountinator : MonoBehaviour
+{
+    public NVBoids flock;
+	public string flockName;
+	
+	private TextMeshProUGUI counter;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+		counter = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter.text = flockName + ": " + flock.birdsNum;
+    }
+}
